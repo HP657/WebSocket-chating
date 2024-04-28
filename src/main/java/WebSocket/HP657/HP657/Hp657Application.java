e9e1919b9aae0f341a84d10c6845b0449fc5fd41
HP657 <junlee5070@gmail.com>
2024-04-28 19:36:20 +0900

    기본 세팅

---
+++ src/main/java/WebSocket/HP657/HP657/Hp657Application.java
@@ -0,0 +1,13 @@
+package WebSocket.HP657.HP657;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hp657Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hp657Application.class, args);
+	}
+
+}
---
+++ src/test/java/WebSocket/HP657/HP657/Hp657ApplicationTests.java
@@ -0,0 +1,13 @@
+package WebSocket.HP657.HP657;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hp657ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
