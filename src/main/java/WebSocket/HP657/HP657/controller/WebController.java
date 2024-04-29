6729a169fa460e5d8b8ed0395011bddca7d4dd2d
HP657 <junlee5070@gmail.com>
2024-04-29 23:00:22 +0900

    기본 컨트롤러

---
+++ src/main/java/WebSocket/HP657/HP657/controller/WebController.java
@@ -0,0 +1,9 @@
+package WebSocket.HP657.HP657.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+}
