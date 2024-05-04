0bef363e6e818ac2885d3e4bd946de45430f3a50
HP657 <junlee5070@gmail.com>
2024-05-04 23:32:46 +0900

    cors설정 현재 왜하는지 모르겠지만 오류가 계속 나는중...

---
+++ src/main/java/WebSocket/HP657/HP657/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package WebSocket.HP657.HP657.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 모든 출처 허용
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
