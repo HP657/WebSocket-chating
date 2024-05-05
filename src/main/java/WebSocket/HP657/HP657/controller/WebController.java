6f52b355cd938da0601afea309a57c4b8bce082e
HP657 <junlee5070@gmail.com>
2024-05-05 17:56:20 +0900

    웹 매핑

--- src/main/java/WebSocket/HP657/HP657/controller/WebController.java
+++ src/main/java/WebSocket/HP657/HP657/controller/WebController.java
@@ -6,4 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/logout")
+    public String logoutPage() {
+        return "logout";
+    }
 }
