2be298319d13b1d5fd318ddf959c3fa065099745
HP657 <junlee5070@gmail.com>
2024-05-09 15:22:53 +0900

    채팅방을 따로 만드어서 초대형식으로 만들어보자 1차시

--- src/main/java/WebSocket/HP657/HP657/controller/ChatController.java
+++ src/main/java/WebSocket/HP657/HP657/controller/ChatController.java
@@ -1,34 +1,40 @@
 package WebSocket.HP657.HP657.controller;
 
+import WebSocket.HP657.HP657.dto.InviteDto;
 import WebSocket.HP657.HP657.dto.MessageDto;
-import WebSocket.HP657.HP657.entity.MessageEntity;
-import WebSocket.HP657.HP657.service.MessageService;
+import WebSocket.HP657.HP657.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ChatController {
-
     @Autowired
-    private MessageService messageService;
+    private ChatService chatService;
 
+    // 채팅 메시지 보내기
     @MessageMapping("/chat.send/{roomId}")
     @SendTo("/topic/public/{roomId}")
-    public MessageDto sendMessage(@Payload MessageDto messageDto, @DestinationVariable String roomId) {
-        MessageEntity savedMessage = messageService.saveMessage(messageDto);
-        return new MessageDto(savedMessage.getUserId(), savedMessage.getContent());
+    public MessageDto sendMessage(@Payload MessageDto messageDto, @DestinationVariable Long roomId) {
+        return chatService.processMessage(messageDto, roomId.toString());
     }
 
+    // 채팅방에 사용자 초대
+    @PostMapping("/chatroom/{roomId}/invite")
+    public ResponseEntity<?> inviteUserToRoom(@PathVariable Long roomId, @RequestBody InviteDto inviteDto) {
+        chatService.inviteUserToRoom(roomId, inviteDto.getUserId());
+        return ResponseEntity.ok().build();
+    }
+
+    // 채팅방 페이지
     @GetMapping("/chatroom/{roomId}")
-    public String chatroom(@PathVariable String roomId, Model model) {
+    public String getChatRoomPage(@PathVariable Long roomId, Model model) {
         model.addAttribute("roomId", roomId);
         return "chatroom";
     }
---
+++ src/main/java/WebSocket/HP657/HP657/dto/InviteDto.java
@@ -0,0 +1,10 @@
+package WebSocket.HP657.HP657.dto;
+
+
+import lombok.Data;
+
+@Data
+public class InviteDto {
+    private Long userId;
+    private Long roomId;
+}
